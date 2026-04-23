e1a9feac0c23afcf67081f465fc89f952cbdbe70
jhones211 <edisong1184@gmail.com>
2026-04-23 17:12:30 -0500

    Se agregagon algunos botones al simulador de nómina

--- php/simulador_de_nomina.php
+++ php/simulador_de_nomina.php
@@ -8,6 +8,17 @@
 <body>
     <h1>SIMULADOR DE NÓMINA</h1>
     <P>Aqui podrás simular tu pre nómina y tener un aproximado de cuanto seria tu próximo de pago de acuerdo a tus horas y días laborados.</P>
+
+    <main>
+        <form action="">
+            <input type="text" placeholder="Periodo a consultar">
+            <input type="text" placeholder="Tipo de quincena">
+
+
+        </form>
+        <li><a href="../php/bienvenida.php">Volver al inicio</a></li>
+        <li><a href="cerrar_sesion.php">Cerrar sesión</a></li>
+    </main>
     
 </body>
 </html>
